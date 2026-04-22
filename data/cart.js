1ca533d0a24ace54b49c4a5c2f4fca0b8db34120
emperorfrost17-png <emperor.frost17@gmail.com>
2026-04-22 06:52:42 +0000

    done with " adds a new product to the cart'"

--- data/cart.js
+++ data/cart.js
@@ -1,19 +1,24 @@
 //The reason why we just use the id is because when using id the id will find the rest of the other details like price rating and image etc. That is why we just only use id and quantity this is called normalizing the data
-export let cart = JSON.parse(localStorage.getItem("cart"));
+export let cart;
+loadFromStorage();
 
-if (!cart) {
-  cart = [
-    {
-      productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-      quantity: 2,
-      deliveryOptionId: "1",
-    },
-    {
-      productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-      quantity: 1,
-      deliveryOptionId: "2",
-    },
-  ];
+export function loadFromStorage() {
+  cart = JSON.parse(localStorage.getItem("cart"));
+
+  if (!cart) {
+    cart = [
+      {
+        productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+        quantity: 2,
+        deliveryOptionId: "1",
+      },
+      {
+        productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+        quantity: 1,
+        deliveryOptionId: "2",
+      },
+    ];
+  }
 }
 
 function saveToStorage() {
--- tests-jasmine/data/cartTest.js
+++ tests-jasmine/data/cartTest.js
@@ -1,17 +1,31 @@
-import { addToCart, cart } from "../../data/cart.js";
+import { addToCart, cart, loadFromStorage } from "../../data/cart.js";
 
 describe("test suite: addToCart", () => {
   it("adds an existing product to the cart", () => {});
   //This is a flaky code because it can work sometimes and sometimes it wouldn't This is because cart depends on what is in the localStorage
   it("adds a new product to the cart", () => {
+    //This is to mock localStorage.setIem
+    spyOn(localStorage, "setItem");
+
     //spyOn is used for mock code
     //spyOn takes two values 1.Object 2.String(Method example getItem)
-    //Now what i did below is to replace localStorage.getItem with what i want basically a fake version
+    //Now what i did below is that I replaced localStorage.getItem with an empty array.
+    //  and reloaded the cart because the cart is saved in localStorage so i have to reload it here for it to be empty after i replace.
+    //  Hope you understand
+    // And don't forget the order of the Code matters
+
     spyOn(localStorage, "getItem").and.callFake(() => {
       return JSON.stringify([]);
     });
-    console.log(localStorage.getItem('cart'))
-    addToCart("e43638ce-6aa0-4b85-b27f-e1d07eb678c6");
+
+    loadFromStorage();
+
+    addToCart("e43638ce-6aa0-4b85-b27f-e1d07eb678c6", 1);
     expect(cart.length).toEqual(1);
+    //.toHaveBeenCalledTimes() This checks how many times localStorage.setItems was called in the code above
+    //I put 1 because i expect it to be called once
+    expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+    expect(cart[0].productId).toEqual("e43638ce-6aa0-4b85-b27f-e1d07eb678c6");
+    expect(cart[0].quantity).toEqual(1);
   });
 });
